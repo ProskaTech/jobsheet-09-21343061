e804f28a528ee2fa2c3c7c717ad3bd29d08f7167
Nazwa Nurfadhilla Phitri <112637218+21343061@users.noreply.github.com>
2022-10-21 14:20:40 +0700

    Add files via upload

---
+++ Praktikum9/src/main/java/satutujuh/ElevatorTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package satutujuh;
+
+/**
+ *
+ * Created by Nazwa Nurfadhilla Phitri
+ */
+public class ElevatorTest {
+    public static void main(String[] args){
+        Elevator myElevator = new Elevator();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goUp();
+        myElevator.goUp();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goDown();
+        myElevator.openDoor();
+        myElevator.closeDoor();
+        myElevator.goDown();
+        myElevator.setFloor(myElevator.TOP_FLOOR);
+        myElevator.openDoor();
+    }
+}
